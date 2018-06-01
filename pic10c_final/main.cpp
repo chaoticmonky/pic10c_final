4f8ed6c91b65e4bf37e97e566c8a0c8db8da82ae
Yugantar Prakash <yugantar@s-169-232-111-201.resnet.ucla.edu>
2018-06-01 01:45:32 -0700

    Initial Commit

---
+++ pic10c_final/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pic10c_final
+//
+//  Created by Yugantar Prakash on 6/1/18.
+//  Copyright © 2018 Yugantar Prakash. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
